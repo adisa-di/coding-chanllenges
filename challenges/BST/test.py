a569b37920dd771c6fe832a0776dcb3cc5ce8576
Adisa Narula <adisan19@gmail.com>
2021-12-11 14:19:46 +0700

    node depth

--- challenges/BST/test.py
+++ challenges/BST/test.py
@@ -1,3 +1,4 @@
+from unittest import result
 import program
 import unittest
 import findSum
@@ -39,6 +40,21 @@ class TestProgram(unittest.TestCase):
 
         expected = [18, 20, 52, 47]
         self.assertEqual(expected, res)
+
+    def test_case_3(self):
+        root = BST(10)  # 0
+        root.left = BST(5)  # 1
+        root.left.left = BST(2)  # 2
+        root.left.left.left = BST(1)  # 3
+        root.left.right = BST(5)  # 2
+        root.right = BST(15)  # 1
+        root.right.left = BST(13)  # 2
+        root.right.left.right = BST(14)  # 3
+        root.right.right = BST(22)  # 2
+
+        res = findSum.nodeDepths(root)
+
+        self.assertEqual(16, res)
 
 
 if __name__ == '__main__':
