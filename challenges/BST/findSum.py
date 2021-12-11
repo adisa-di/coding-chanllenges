6848beeec49ba44d6ff69e8f12020426f9b6ee54
Adisa Narula <adisan19@gmail.com>
2021-12-11 14:48:51 +0700

    add iterative and cleaned up test

--- challenges/BST/findSum.py
+++ challenges/BST/findSum.py
@@ -40,3 +40,19 @@ def nodeHelper(root, depth=0):
     if (root is None):
         return 0
     return depth + nodeHelper(root.left, depth + 1) + nodeHelper(root.right, depth + 1)
+
+
+def nodeDepthsIterative(root):
+    sumOfDepths = 0
+    stack = [{"node": root, "depth": 0}]
+    while len(stack) > 0:
+        nodeInfo = stack.pop()
+        node, depth = nodeInfo["node"], nodeInfo["depth"]
+        if node is None:
+            continue
+
+        sumOfDepths += depth
+        stack.append({"node": node.left, "depth": depth + 1})
+        stack.append({"node": node.right, "depth": depth + 1})
+
+    return sumOfDepths
--- challenges/BST/test.py
+++ challenges/BST/test.py
@@ -12,7 +12,8 @@ class BST:
 
 
 class TestProgram(unittest.TestCase):
-    def test_case_1(self):
+
+    def getTree(self):
         root = BST(10)
         root.left = BST(5)
         root.left.left = BST(2)
@@ -22,39 +23,29 @@ class TestProgram(unittest.TestCase):
         root.right.left = BST(13)
         root.right.left.right = BST(14)
         root.right.right = BST(22)
+
+        return root
+
+    def test_case_1(self):
+        root = self.getTree()
         expected = 13
         actual = program.findClosestValueInBst(root, 12)
         self.assertEqual(expected, actual)
 
     def test_case_2(self):
-        root = BST(10)
-        root.left = BST(5)
-        root.left.left = BST(2)
-        root.left.left.left = BST(1)
-        root.left.right = BST(5)
-        root.right = BST(15)
-        root.right.left = BST(13)
-        root.right.left.right = BST(14)
-        root.right.right = BST(22)
+        root = self.getTree()
         res = findSum.branchSums(root)
 
         expected = [18, 20, 52, 47]
         self.assertEqual(expected, res)
 
     def test_case_3(self):
-        root = BST(10)  # 0
-        root.left = BST(5)  # 1
-        root.left.left = BST(2)  # 2
-        root.left.left.left = BST(1)  # 3
-        root.left.right = BST(5)  # 2
-        root.right = BST(15)  # 1
-        root.right.left = BST(13)  # 2
-        root.right.left.right = BST(14)  # 3
-        root.right.right = BST(22)  # 2
-
+        root = self.getTree()
         res = findSum.nodeDepths(root)
+        resIt = findSum.nodeDepths(root)
 
         self.assertEqual(16, res)
+        self.assertEqual(16, resIt)
 
 
 if __name__ == '__main__':
