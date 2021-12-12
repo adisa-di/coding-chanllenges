266386ea68c4e25d39cfe0f37b62a049ec301211
Adisa Narula <adisan19@gmail.com>
2021-12-12 12:56:38 +0700

    tandem 2

--- challenges/Arrays/tandem.py
+++ challenges/Arrays/tandem.py
@@ -9,3 +9,18 @@ def tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest):
         summation += max(redShirtSpeeds[i], blueShirtSpeeds[blueIdx])
 
     return summation
+
+
+def tandemBicycle2(redShirtSpeeds, blueShirtSpeeds, fastest):
+    length = len(redShirtSpeeds)
+    redShirtSpeeds.sort()
+    blueShirtSpeeds.sort()
+
+    if fastest:
+        blueShirtSpeeds.reverse()
+
+    summation = 0
+    for i in range(length):
+        summation += max(redShirtSpeeds[i], blueShirtSpeeds[i])
+
+    return summation
--- challenges/Arrays/test.py
+++ challenges/Arrays/test.py
@@ -12,6 +12,15 @@ class TestProgram(unittest.TestCase):
             redShirtSpeeds, blueShirtSpeeds, fastest)
         self.assertEqual(actual, expected)
 
+    def test_case_2(self):
+        redShirtSpeeds = [5, 5, 3, 9, 2]
+        blueShirtSpeeds = [3, 6, 7, 2, 1]
+        fastest = True
+        expected = 32
+        actual = tandem.tandemBicycle2(
+            redShirtSpeeds, blueShirtSpeeds, fastest)
+        self.assertEqual(actual, expected)
+
 
 if __name__ == "__main__":
     unittest.main()
