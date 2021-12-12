ff1c918952a2a6de4a26c219de995c3172246475
Adisa Narula <adisan19@gmail.com>
2021-12-12 19:39:13 +0700

    ana

--- challenges/Fib/fac.py
+++ challenges/Fib/fac.py
@@ -1,12 +1,33 @@
 def factorial(n):
 
-  ans = 1
+    ans = 1
 
-  while n >= 1:
-    ans *= n
-    n-=1
-  
-  return ans
-  
+    while n >= 1:
+        ans *= n
+        n -= 1
 
-  
+    return ans
+
+
+def anagrams(words):
+    hash = {}
+    for word in words:
+        letters = list(word)
+        letters.sort()
+        sorted = ''.join(letters)
+
+        # get array in hash
+        arr = None
+        if sorted in hash:
+            arr = hash[sorted]
+        else:
+            arr = []
+
+        arr.append(word)
+        hash[sorted] = arr
+
+    result = []
+    for key in hash:
+        result.append(hash[key])
+
+    return result
